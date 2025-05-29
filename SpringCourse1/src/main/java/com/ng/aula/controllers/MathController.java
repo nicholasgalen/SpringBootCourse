5664224770e080c0b8123f38e885136250e1de5b
Nicholas Galen <galennicholas@proton.me>
2025-05-29 14:47:00 -0400

    Create simple math controller
    Math controller that accepts and validates two doubles as parameters in the URI using regex and replacing coma with dot. Returns the sum of two numbers.

---
+++ SpringCourse1/src/main/java/com/ng/aula/controllers/MathController.java
@@ -0,0 +1,42 @@
+package com.ng.aula.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math") // aplica em todos os componentes dentro do math controller
+public class MathController {
+    // math/sum
+    @RequestMapping("/sum/{num1}/{num2}")
+    public Double sum(
+            @PathVariable("num1") String num1,
+            @PathVariable("num2") String num2
+    ) throws Exception{
+        if (!isNumeric(num1) || !isNumeric(num2)) throw new IllegalArgumentException();
+        return convertToDouble(num1) + convertToDouble(num2);
+    }
+
+    private Double convertToDouble(String strNum) {
+        // Mesma validação do isNumeric
+        if (strNum == null || strNum.isEmpty()) throw new IllegalArgumentException();
+        String number = strNum.replace(",", ".");
+
+        return Double.parseDouble(number);
+    }
+
+    private boolean isNumeric(String strNum) {
+        // Verifica se o numero é null ou empty
+        if (strNum == null || strNum.isEmpty()) return false;
+
+        // Substitui a virgula por ponto (no brasil usa-se virgula para dinheiro ou decilmal
+        // eg. R$ 3,54 enquanto a convenção é ponto, eg. USD 3.54
+        String number = strNum.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
+    }
+
+
+    // math/subtraction
+    // math/division
+    // math/multiplication
+}
